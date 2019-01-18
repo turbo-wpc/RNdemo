4c4cea3802ac486fdb73855078b63d9721afabe8
wangpengchong <wangpengchong@autohome.com.cn>
2019-01-18 18:25:04 +0800

    手势

--- README.md
+++ README.md
@@ -30,14 +30,10 @@ View.props.onStartShouldSetPanResponder: (evt, gestureState) => true
 // 该属性接受一个回调函数，如果返回true则申请成为滑动过程中的响应者
 View.props.onMoveShouldSetPanResponder: (evt, gestureState) => true
 ```
-在实际的代码中，我们经常通过以下方式设置PanResponder来申请和监听触摸事件的
-```javascript
-View.props.onStartShouldSetPanResponder: (evt, gestureState) => true,
-View.props.onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-View.props.onMoveShouldSetPanResponder: (evt, gestureState) => true,
-View.props.onMoveShouldSetPanResponderCapture: (evt, gestureState) => true
-```
-其中
+#### 2、捕获 ShouldSet 事件处理
+onStartShouldSetPanResponder与onMoveShouldSetPanResponder是以冒泡的形式调用的，即嵌套最深的节点最先调用。这意味着当多个 View 同时在*ShouldSetResponder中返回 true 时，最底层的 View 将优先“夺权”。在多数情况下这并没有什么问题，因为这样可以确保所有控件和按钮是可用的。
+
+但是有些时候，某个父 View 会希望能先成为响应者。我们可以利用“捕获期”来解决这一需求。响应系统在从最底层的组件开始冒泡之前，会首先执行一个“捕获期”，在此期间会触发on*StartShouldSetPanResponderCapture系列事件。因此，如果某个父 View 想要在触摸操作开始时阻止子组件成为响应者，那就应该处理onStartShouldSetResponderCapture事件并返回 true 值。
 ```javascript
 // 表示是否成为触摸事件的劫持者，返回true，则不会把事件传递给它的子元素
 View.props.onStartShouldSetPanResponderCapture: (evt, gestureState) => true
@@ -45,6 +41,14 @@ View.props.onStartShouldSetPanResponderCapture: (evt, gestureState) => true
 ```javascript
 // 表示是否成为滑动事件的劫持者，返回true，则不会把滑动事件传递给它的子元素
 View.props.onMoveShouldSetPanResponder: (evt, gestureState) => true
+```
+
+在实际的代码中，我们经常通过以下方式设置PanResponder来申请和监听触摸事件的
+```javascript
+View.props.onStartShouldSetPanResponder: (evt, gestureState) => true,
+View.props.onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+View.props.onMoveShouldSetPanResponder: (evt, gestureState) => true,
+View.props.onMoveShouldSetPanResponderCapture: (evt, gestureState) => true
 ```
 
 #### 2、开始手势操作
--- src/components/Touch.js
+++ src/components/Touch.js
@@ -11,15 +11,19 @@ export default class Touch extends PureComponent {
   constructor(props) {
     super(props)
     this.state = {
-      redViewBgColor: 'red'
+      redViewBgColor: 'red',
+      childBgColor: 'yellow'
     }
+    this._changeChild = this._changeChild.bind(this)
+    this._unhighlight = this._unhighlight.bind(this)
+    this._highlight = this._highlight.bind(this)
   }
 
   componentWillMount(){
     this._panResponder = PanResponder.create({
       // 要求成为响应者：相应的回调函数返回值为 true
       onStartShouldSetPanResponder: (evt, gestureState) => true,
-      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => false,
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
 
@@ -49,6 +53,39 @@ export default class Touch extends PureComponent {
         return true
       }
     })
+
+    this._childPanResponder = PanResponder.create({
+      // 要求成为响应者：相应的回调函数返回值为 true
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        // 开始手势操作。给用户一些视觉反馈，让他们知道发生了什么事情！
+        // gestureState.{x,y} 现在会被设置为0
+        this._changeChild()
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        // 最近一次的移动距离为gestureState.move{X,Y}
+        // 从成为响应者开始时的累计手势移动距离为gestureState.d{x,y}
+        console.warn(evt.nativeEvent.locationX)
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: (evt, gestureState) => {
+        // 用户放开了所有的触摸点，且此时视图已经成为了响应者
+        // 一般来说这意味着一个手势操作已经成功完成
+        this._unhighlight()
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        // 另一个组件已经成为了新的响应者，所以当前手势将被取消。
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        // 返回一个布尔值，决定当前组件是否应该阻止原生组件成为JS响应者
+        // 默认返回true。目前暂时只支持android
+        return true
+      }
+    })
   }
 
   _unhighlight(){
@@ -57,6 +94,12 @@ export default class Touch extends PureComponent {
 
   _highlight(){
     this.setState({redViewBgColor: 'blue'})
+    console.warn(6)
+  }
+
+  _changeChild(){
+    // this.setState({childBgColor: 'grey'})
+    console.warn(8)
   }
 
   render() {
@@ -64,7 +107,11 @@ export default class Touch extends PureComponent {
       <View style={styles.container}>
         <View style={[styles.redView,{backgroundColor: this.state.redViewBgColor}]}
             {...this._panResponder.panHandlers}
-        ></View>
+        >
+          <View style={[styles.child,{backgroundColor: this.state.childBgColor}]}
+              {...this._childPanResponder.panHandlers}
+          ></View>
+        </View>
       </View>
     )
   }
@@ -75,9 +122,15 @@ const styles = StyleSheet.create({
     flex: 1
   },
   redView: {
+    alignItems: 'center',
+    justifyContent: 'center',
     width: 100,
     height: 100,
     marginTop: 100,
     marginLeft: 100
+  },
+  child: {
+    width: 50,
+    height: 50
   }
 })
